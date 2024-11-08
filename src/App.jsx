07eb73ae0f0fc9b9252b5fbee110925948dd9585
KaoParunyu <kao192837@gmail.com>
2024-11-08 14:27:05 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+ import Sidebar from "./componant/Sidebar";
+function App() {
+  return (
+    <section>
+      <div>
+      <Sidebar/>
+      </div>
+    </section>
+  );
+}
+
+export default App;
